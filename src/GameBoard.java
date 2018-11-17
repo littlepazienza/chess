44511fa923520cfb9c682d4b239d720ffa7d5d91
bjpazienza <pazeinza.log@gmail.com>
2018-11-16 20:44:20 -0500

    shsg

---
+++ src/GameBoard.java
@@ -0,0 +1,15 @@
+public class GameBoard
+{
+
+	protected int[][] board;
+
+	public GameBoard()
+	{
+		board = new int[8][8];
+	
+		
+	}
+
+	
+
+}
--- src/Piece.java
+++ src/Piece.java
@@ -1,13 +1,20 @@
 public abstract class Piece
 {
 	protected int file, row;
+	protected Side color;
 	
-	public Piece(int f, int r)
+	public Piece(int f, int r, Side c)
 	{
 		this.file = f;
 		this.row = r;
+		this.color = c;
 	}
 
-	public boolean validMove(int f, int r);
+	public abstract boolean validMove(int f, int r);
 
+
+	public enum Side
+	{
+		WHITE, BLACK
+	}
 }
