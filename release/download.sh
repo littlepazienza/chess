d6e01b76366a510224deac2317b57c6bc19f6f12
Ben <pazienza.log@gmail.com>
2019-09-07 12:03:49 -0400

    script to run the downloads

---
+++ release/download.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CHESS_DIR=/home/ben/chess/release
+DEPLOY=deployments
+DNLD=downloads
+
+echo Bensdaman1* | sudo rm -rf $CHESS_DIR/$DEPLOY/* $CHESS_DIR/$DNLD/*
+cd $CHESS_DIR
+git checkout master
+git pull
+npm install
+electron-packager . chess --all
+
+mv $CHESS_DIR/chess-* $CHESS_DIR/$DEPLOY
+VERSION=($(ls $CHESS_DIR/$DEPLOY))
+
+for i in "${VERSION[@]}"
+do
+	echo zipping $i
+	zip -r $CHESS_DIR/$DNLD/$i.zip $CHESS_DIR/$DEPLOY/$i
+done
